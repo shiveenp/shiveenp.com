a3a8d77e991508494dd493c8e54a7592d8a6de90
shiveenpandita <shiveenpandita@fastmail.com>
2024-01-29 13:34:31 +1100

    Revert "Moved to hugo"
    This reverts commit ca263d929af7f2e615ad77a11e828d60c7814c38.

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Shiveen Pandita",
+	url: "https://shiveenp.com/",
+	language: "en",
+	description: "I write about things that interest me in the moment",
+	author: {
+		name: "Shiveen Pandita",
+		email: "blog@shiveenp.com",
+		url: "https://shiveenp.com.com/about-me/"
+	}
+}
